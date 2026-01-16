3fb32f781b0b830207bf9e90ea40b8ea914d726d
gleicysanrocha <107890714+gleicysanrocha@users.noreply.github.com>
2026-01-16 16:38:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'gdn-os-v1';
+const CACHE_NAME = 'gdn-os-v2';
 const ASSETS = [
-    'index.html',
-    'css/main.css',
-    'js/app.js',
-    'js/storage.js',
-    'js/modules/os.js',
-    'js/modules/clients.js',
-    'js/modules/financial.js',
-    'js/modules/settings.js',
-    'assets/img/logo.png',
+    './',
+    './index.html',
+    './css/main.css',
+    './js/app.js',
+    './js/storage.js',
+    './js/modules/os.js',
+    './js/modules/clients.js',
+    './js/modules/financial.js',
+    './js/modules/settings.js',
+    './assets/img/logo.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js'
 ];
