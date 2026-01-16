d4648bdb19f7651f0750f4226b4172ba4f602d57
gleicysanrocha <107890714+gleicysanrocha@users.noreply.github.com>
2026-01-16 16:48:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,11 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS).catch(err => {
+                console.warn('Alguns arquivos não puderam ser cacheados:', err);
+            });
+        })
     );
 });
 
