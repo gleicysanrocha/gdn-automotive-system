5eb2997d271030b9074337e697edb3e9c6fe54ca
gleicysanrocha <107890714+gleicysanrocha@users.noreply.github.com>
2026-07-31 13:05:01 -0300

    fix: inline clear filters button, convert special chars to HTML entities to fix encoding bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gdn-os-v37';
+const CACHE_NAME = 'gdn-os-v38';
 const ASSETS = [
     './',
     './index.html',
@@ -8,7 +8,7 @@ const ASSETS = [
     './js/firebase-config.js?v=2.8',
     './js/modules/auth.js?v=2.8',
     './js/modules/nfse.js?v=2.19',
-    './js/modules/os.js?v=2.31',
+    './js/modules/os.js?v=2.32',
     './js/modules/clients.js?v=2.8',
     './js/modules/technicians.js?v=2.8',
     './js/modules/financial.js?v=2.22',
